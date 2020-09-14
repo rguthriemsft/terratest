e4bd0500121a74cab1800fc1640bf5e3dbc48b03
wmattlong <malong@microsoft.com>
2020-09-14 09:25:17 -0500

    kv files

---
+++ test/azure/keyvault_test.go
@@ -0,0 +1,37 @@
+// +build azure
+
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/azure"
+	"github.com/stretchr/testify/require"
+)
+
+func TestKeyVaultSecretExists(t *testing.T) {
+	t.Parallel()
+
+	testKeyVaultName := "fakeKeyVault"
+	testKeyVaultSecretName := "fakeSecretName"
+	_, err := azure.KeyVaultSecretExistsE(testKeyVaultName, testKeyVaultSecretName)
+	require.Error(t, err)
+}
+
+func TestKeyVaultKeyExists(t *testing.T) {
+	t.Parallel()
+
+	testKeyVaultName := "fakeKeyVault"
+	testKeyVaultKeyName := "fakeKeyName"
+	_, err := azure.KeyVaultKeyExistsE(testKeyVaultName, testKeyVaultKeyName)
+	require.Error(t, err)
+}
+
+func TestKeyVaultCertificateExists(t *testing.T) {
+	t.Parallel()
+
+	testKeyVaultName := "fakeKeyVault"
+	testKeyVaultCertName := "fakeCertName"
+	_, err := azure.KeyVaultCertificateExistsE(testKeyVaultName, testKeyVaultCertName)
+	require.Error(t, err)
+}

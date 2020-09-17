308fccf096fccf0ac83949797eb91af1e08ade06
wmattlong <malong@microsoft.com>
2020-09-17 17:31:33 -0500

    rm azure ref

--- modules/azure/loganalytics_test.go
+++ modules/azure/loganalytics_test.go
@@ -1,40 +1,28 @@
-// +build azure
-
-// NOTE: We use build tags to differentiate azure testing because we currently do not have azure access setup for
-// CircleCI.
-
-package test
+package azure
 
 import (
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/stretchr/testify/assert"
 )
-
-/*
-The below tests are currently stubbed out, with the expectation that they will throw errors.
-If/when CRUD methods are introduced for Azure Virtual Machines, these tests can be extended
-(see AWS S3 tests for reference).
-*/
 
 func TestLogAnalyticsWorkspace(t *testing.T) {
 	t.Parallel()
 
-	result := azure.LogAnalyticsWorkspaceExists("", "", "")
+	result := LogAnalyticsWorkspaceExists("", "", "")
 	assert.False(t, result)
 }
 
 func TestLogAnalyticsSku(t *testing.T) {
 	t.Parallel()
 
-	result := azure.GetLogAnalyticsWorkspaceSku("", "", "")
+	result := GetLogAnalyticsWorkspaceSku("", "", "")
 	assert.Equal(t, "", result)
 }
 
 func TestLogAnalyticsRetentionPeriodDays(t *testing.T) {
 	t.Parallel()
 
-	result := azure.GetLogAnalyticsWorkspaceRetentionPeriodDays("", "", "")
+	result := GetLogAnalyticsWorkspaceRetentionPeriodDays("", "", "")
 	assert.Equal(t, int32(-1), result)
 }

cb5952fe5cdb641fde255388f6cb083876cdc95c
wmattlong <malong@microsoft.com>
2020-09-17 17:29:20 -0500

    rm azure ref

--- modules/azure/storage_test.go
+++ modules/azure/storage_test.go
@@ -3,36 +3,35 @@ package azure
 import (
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/stretchr/testify/require"
 )
 
 func TestStorageAccountExists(t *testing.T) {
-	_, err := azure.StorageAccountExistsE("", "", "")
+	_, err := StorageAccountExistsE("", "", "")
 	require.Error(t, err)
 }
 
 func TestStorageBlobContainerExists(t *testing.T) {
-	_, err := azure.StorageBlobContainerExistsE("", "", "", "")
+	_, err := StorageBlobContainerExistsE("", "", "", "")
 	require.Error(t, err)
 }
 
 func TestStorageBlobContainerPublicAccess(t *testing.T) {
-	_, err := azure.GetStorageBlobContainerPublicAccessE("", "", "", "")
+	_, err := GetStorageBlobContainerPublicAccessE("", "", "", "")
 	require.Error(t, err)
 }
 
 func TestGetStorageAccountKind(t *testing.T) {
-	_, err := azure.GetStorageAccountKindE("", "", "")
+	_, err := GetStorageAccountKindE("", "", "")
 	require.Error(t, err)
 }
 
 func TestGetStorageAccountSkuTier(t *testing.T) {
-	_, err := azure.GetStorageAccountSkuTierE("", "", "")
+	_, err := GetStorageAccountSkuTierE("", "", "")
 	require.Error(t, err)
 }
 
 func TestGetStorageDNSString(t *testing.T) {
-	_, err := azure.GetStorageDNSStringE("", "", "")
+	_, err := GetStorageDNSStringE("", "", "")
 	require.Error(t, err)
 }

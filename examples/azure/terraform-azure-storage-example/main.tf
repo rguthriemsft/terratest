98e7a37fcdf8adfdb7d8fd329ab24f10f7aca375
wmattlong <malong@microsoft.com>
2020-09-17 16:11:05 -0500

    move unit test update prefix

--- examples/azure/terraform-azure-storage-example/main.tf
+++ examples/azure/terraform-azure-storage-example/main.tf
@@ -1,37 +1,25 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# ENVIRONMENT VARIABLES
-# Define these secrets as environment variables
-# ---------------------------------------------------------------------------------------------------------------------
-
-# ARM_CLIENT_ID
-# ARM_CLIENT_SECRET
-# ARM_SUBSCRIPTION_ID
-# ARM_TENANT_ID
-
-# ---------------------------------------------------------------------------------------------------------------------
-# REQUIRED PARAMETERS
-# You must provide a value for each of these parameters.
-# ---------------------------------------------------------------------------------------------------------------------
-
-# ---------------------------------------------------------------------------------------------------------------------
-# OPTIONAL PARAMETERS
-# These parameters have reasonable defaults.
-# ---------------------------------------------------------------------------------------------------------------------
-
-resource "random_string" "default" {
+resource "random_string" "storedefault" {
   length = 8  
   lower = true
   number = false
   special = false
 }
 
+resource "random_string" "default" {
+  length = 3  
+  lower = true
+  number = false
+  special = false
+}
+
 resource "azurerm_resource_group" "main" {
-  name     =  "${var.prefix}-resources"
+  name     =  format("%s-%s-%s", "terratest", lower(random_string.default.result), "storage")
   location = var.location
 }
 
+
 resource "azurerm_storage_account" "storageaccount" {
-  name                     = lower(random_string.default.result)
+  name                     = lower(random_string.storedefault.result)
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_kind             = "StorageV2"
--- examples/azure/terraform-azure-storage-example/variables.tf
+++ examples/azure/terraform-azure-storage-example/variables.tf
@@ -1,10 +1,4 @@
 
-variable "prefix" {
-  description = "The prefix that will be attached to all resources deployed."
-  type        = string
-  default     = "testprefix"
-}
-
 variable "location" {
   description = "The location to set for the storage account."
   type        = string

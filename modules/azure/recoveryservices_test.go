c4e2d6911756399dfa98a9451cd1d259c0212a35
wmattlong <malong@microsoft.com>
2020-09-17 17:33:31 -0500

    rm azure ref

--- modules/azure/recoveryservices_test.go
+++ modules/azure/recoveryservices_test.go
@@ -3,22 +3,21 @@ package azure
 import (
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestRecoveryServicesVaultName(t *testing.T) {
-	exists := azure.RecoveryServicesVaultExists("", "", "")
+	exists := RecoveryServicesVaultExists("", "", "")
 	assert.False(t, exists, "vault does not exist")
 }
 
 func TestRecoveryServicesVaultBackupPolicyList(t *testing.T) {
-	_, err := azure.GetRecoveryServicesVaultBackupPolicyListE("", "", "")
+	_, err := GetRecoveryServicesVaultBackupPolicyListE("", "", "")
 	require.Error(t, err, "Backup policy list not faulted")
 }
 
 func TestRecoveryServicesVaultBackupProtectedVMList(t *testing.T) {
-	_, err := azure.GetRecoveryServicesVaultBackupProtectedVMListE("", "", "", "")
+	_, err := GetRecoveryServicesVaultBackupProtectedVMListE("", "", "", "")
 	require.Error(t, err, "Backup policy protected vm list not faulted")
 }

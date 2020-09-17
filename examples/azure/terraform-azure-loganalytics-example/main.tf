c1e14f0cae29ac2724933e5a4f5b188c27be5a5e
wmattlong <malong@microsoft.com>
2020-09-17 16:16:50 -0500

    chg terraform rg prefix

--- examples/azure/terraform-azure-loganalytics-example/main.tf
+++ examples/azure/terraform-azure-loganalytics-example/main.tf
@@ -1,17 +1,24 @@
 resource "random_string" "default" {
-  length = 8  
+  length = 3  
   lower = true
   number = false
   special = false
 }
 
 resource "azurerm_resource_group" "main" {
-  name     =  "${var.prefix}-resources"
+  name     =  format("%s-%s-%s", "terratest", lower(random_string.default.result), "loganalytics")
   location = var.location
 }
 
+resource "random_string" "defaultkv" {
+  length = 8  
+  lower = true
+  number = false
+  special = false
+}
+
 resource "azurerm_log_analytics_workspace" "logws" {
-  name                = lower(random_string.default.result)
+  name                = lower(random_string.defaultkv.result)
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   sku                 = "PerGB2018"

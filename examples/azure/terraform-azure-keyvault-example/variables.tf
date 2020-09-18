8f0a3281e1c2ddf336a862b4e09a4b21fd3c6139
wmattlong <malong@microsoft.com>
2020-09-18 13:02:56 -0500

    refactor test

--- examples/azure/terraform-azure-keyvault-example/variables.tf
+++ examples/azure/terraform-azure-keyvault-example/variables.tf
@@ -1,26 +1,38 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# ENVIRONMENT VARIABLES
-# Define these secrets as environment variables
-# ---------------------------------------------------------------------------------------------------------------------
-
-# ARM_CLIENT_ID
-# ARM_CLIENT_SECRET
-# ARM_SUBSCRIPTION_ID
-# ARM_TENANT_ID
-
-# ---------------------------------------------------------------------------------------------------------------------
-# REQUIRED PARAMETERS
-# You must provide a value for each of these parameters.
-# ---------------------------------------------------------------------------------------------------------------------
-
-# ---------------------------------------------------------------------------------------------------------------------
-# OPTIONAL PARAMETERS
-# These parameters have reasonable defaults.
-# ---------------------------------------------------------------------------------------------------------------------
 
 variable "location" {
   description = "The location to set for the storage account."
   type        = string
   default     = "East US"
 }
+
+variable "resource_group_name" {
+  description = "The name to set for the resource group."
+  type        = string
+  default     = "azure-keyvault-test"
+}
+
+variable "key_vault_name" {
+  description = "The name to set for the key vault."
+  type        = string
+  default     = "terratestkeyvault"
+}
+
+variable "secret_name" {
+  description = "The name to set for the key vault secret."
+  type        = string
+  default     = "secret1"
+}
+
+variable "key_name" {
+  description = "The name to set for the key vault key."
+  type        = string
+  default     = "key1"
+}
+
+variable "certificate_name" {
+  description = "The name to set for the key vault certificate."
+  type        = string
+  default     = "certificate1"
+}
+
 

a68792efb3db9d2258e6d362f8b43d809e23b150
wmattlong <malong@microsoft.com>
2020-09-18 11:10:48 -0500

    refactor test

--- examples/azure/terraform-azure-loganalytics-example/main.tf
+++ examples/azure/terraform-azure-loganalytics-example/main.tf
@@ -1,26 +1,13 @@
-resource "random_string" "default" {
-  length = 3  
-  lower = true
-  number = false
-  special = false
-}
 
-resource "azurerm_resource_group" "main" {
-  name     =  format("%s-%s-%s", "terratest", lower(random_string.default.result), "loganalytics")
+resource "azurerm_resource_group" "example" {
+  name     =  var.resource_group_name
   location = var.location
 }
 
-resource "random_string" "defaultkv" {
-  length = 8  
-  lower = true
-  number = false
-  special = false
-}
-
-resource "azurerm_log_analytics_workspace" "logws" {
-  name                = lower(random_string.defaultkv.result)
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+resource "azurerm_log_analytics_workspace" "example" {
+  name                = var.loganalytics_workspace_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
--- examples/azure/terraform-azure-loganalytics-example/variables.tf
+++ examples/azure/terraform-azure-loganalytics-example/variables.tf
@@ -4,3 +4,15 @@ variable "location" {
   type        = string
   default     = "East US"
 }
+
+variable "resource_group_name" {
+  description = "The name to set for the resource group."
+  type        = string
+  default     = "loganalytics"
+}
+
+variable "loganalytics_workspace_name" {
+  description = "The name to set for the log analytics workspace."
+  type        = string
+  default     = "loganalyticsws"
+}

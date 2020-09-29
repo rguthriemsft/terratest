8d9bc8e37754e61ca92d4295bc0f10c24232bfa1
wmattlong <malong@microsoft.com>
2020-09-29 08:44:11 -0500

    add comment

--- modules/azure/storage_test.go
+++ modules/azure/storage_test.go
@@ -6,6 +6,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+/*
+The below tests are currently stubbed out, with the expectation that they will throw errors.
+If/when CRUD methods are introduced for Azure Virtual Machines, these tests can be extended
+(see AWS S3 tests for reference).
+*/
+
 func TestStorageAccountExists(t *testing.T) {
 	_, err := StorageAccountExistsE("", "", "")
 	require.Error(t, err)

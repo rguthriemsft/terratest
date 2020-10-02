6d36c6e24e24506123d7963afed47738f52d33c8
wmattlong <malong@microsoft.com>
2020-10-02 14:46:32 -0500

    unit test

--- modules/azure/loganalytics_test.go
+++ modules/azure/loganalytics_test.go
@@ -14,20 +14,20 @@ If/when methods to create and delete log analytics resources are added, these te
 func TestLogAnalyticsWorkspace(t *testing.T) {
 	t.Parallel()
 
-	result := LogAnalyticsWorkspaceExists("", "", "")
+	result := LogAnalyticsWorkspaceExists(t, "", "", "")
 	assert.False(t, result)
 }
 
 func TestLogAnalyticsSku(t *testing.T) {
 	t.Parallel()
 
-	result := GetLogAnalyticsWorkspaceSku("", "", "")
+	result := GetLogAnalyticsWorkspaceSku(t, "", "", "")
 	assert.Equal(t, "", result)
 }
 
 func TestLogAnalyticsRetentionPeriodDays(t *testing.T) {
 	t.Parallel()
 
-	result := GetLogAnalyticsWorkspaceRetentionPeriodDays("", "", "")
+	result := GetLogAnalyticsWorkspaceRetentionPeriodDays(t, "", "", "")
 	assert.Equal(t, int32(-1), result)
 }
